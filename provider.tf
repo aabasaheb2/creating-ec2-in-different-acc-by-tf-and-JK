63dee75d453410e2fc4423fe4db7c8ced4f9b1d1
aabasaheb2 <aabasahebjadhav22@gmail.com>
2025-06-20 13:17:15 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,5 @@
 provider "aws" {
-  alias  = "account1"
-  region = "ap-southeast-1"
-  access_key = "AKIAVWABKDZWLIMC3GUU"
-  secret_key = "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"
+  alias   = "account_a"
+  profile = "account-a-profile"
+  region  = var.region_a
 }
